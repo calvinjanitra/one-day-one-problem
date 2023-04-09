bde40492b487a4089889a22dbb591d561745aa56
Calvin Janitra <calvinjanitra9@gmail.com>
2023-04-09 16:38:45 +0700

    add day 1

---
+++ 1_spoj_65080.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int reverse(int x)
+{
+    long long res = 0;
+    while (x)
+    {
+        res *= 10;
+        res += x % 10;
+        x /= 10;
+    }
+    return res;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    long long a, b, res;
+
+    while (n--)
+    {
+        cin >> a >> b;
+        res = reverse(reverse(a) + reverse(b));
+        cout << res << endl;
+    }
+
+    return 0;
+}
